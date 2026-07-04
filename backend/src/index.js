01f2da4487bc75be5740647f6acc7a8503c34250
rajendraxD <rajendraxd1@gmail.com>
2026-07-05 01:21:53 +0530

    feat: return JSON response from root route

--- backend/src/index.js
+++ backend/src/index.js
@@ -3,9 +3,9 @@ import express from "express";
 const app = express();
 
 app.get("/", (req, res) => {
-    res.send("Hello World!");
+  return res.json({ message: "Server is running..." });
 });
 
 app.listen(3000, () => {
-    console.log("Example app listening on port 3000!");
+  console.log("Example app listening on port 3000!");
 });
