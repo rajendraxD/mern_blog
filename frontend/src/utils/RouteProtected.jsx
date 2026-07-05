9a9e713bd5925e0a3800528641eda2e2b2ad35f7
rajendraxD <rajendraxd1@gmail.com>
2026-07-06 04:28:52 +0530

    refactor: replace inline Loading component with LoadingSpinner

--- frontend/src/utils/RouteProtected.jsx
+++ frontend/src/utils/RouteProtected.jsx
@@ -1,13 +1,12 @@
 import { useSelector } from "react-redux";
 import { Navigate, Outlet } from "react-router-dom";
-
-const Loading = () => <h2>Loading</h2>;
+import LoadingSpinner from "../components/common/LoadingSpinner";
 
 export const PublicRoute = () => {
   const { user, isLoading } = useSelector((state) => state.auth);
   const { isLoading: userLoading } = useSelector((state) => state.user);
   if (isLoading || userLoading) {
-    return <Loading />;
+    return <LoadingSpinner />;
   }
   if (user) {
     return <Navigate to="/dashboard" replace />;
@@ -19,7 +18,7 @@ export const ProtectedRoute = () => {
   const { user, isLoading } = useSelector((state) => state.auth);
   const { isLoading: userLoading } = useSelector((state) => state.user);
   if (isLoading || userLoading) {
-    return <Loading />;
+    return <LoadingSpinner />;
   }
   if (user) {
     return <Outlet />;
