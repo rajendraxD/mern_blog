0f84bd36b4b09c6c4ef7c2a89d407bfa8c0bd77a
rajendraxD <rajendraxd1@gmail.com>
2026-07-05 10:30:44 +0530

    feat: add LoginForm component with Google icon and react-icons dep

---
+++ frontend/src/pages/features/auth/login/LoginForm.jsx
@@ -0,0 +1,58 @@
+import { cn } from "@/lib/utils"
+import { Button } from "@/components/ui/button"
+import {
+    Field,
+    FieldDescription,
+    FieldGroup,
+    FieldLabel,
+    FieldSeparator,
+} from "@/components/ui/field"
+import { Input } from "@/components/ui/input"
+import { FcGoogle } from "react-icons/fc";
+
+export function LoginForm() {
+    return (
+        <form className={cn("flex flex-col gap-6")}>
+            <FieldGroup>
+                <div className="flex flex-col items-center gap-1 text-center">
+                    <h1 className="text-2xl font-bold">Login to your account</h1>
+                    <p className="text-sm text-balance text-muted-foreground">
+                        Enter your email below to login to your account
+                    </p>
+                </div>
+                <Field>
+                    <FieldLabel htmlFor="email">Email</FieldLabel>
+                    <Input id="email" type="email" placeholder="username@gmail.com" required />
+                </Field>
+                <Field>
+                    <div className="flex items-center">
+                        <FieldLabel htmlFor="password">Password</FieldLabel>
+                        <a
+                            href="#"
+                            className="ml-auto text-sm underline-offset-4 hover:underline"
+                        >
+                            Forgot password?
+                        </a>
+                    </div>
+                    <Input id="password" type="password" placeholder="********" required />
+                </Field>
+                <Field>
+                    <Button type="submit">Login</Button>
+                </Field>
+                <FieldSeparator>Or continue with</FieldSeparator>
+                <Field>
+                    <Button variant="outline" type="button">
+                        <FcGoogle className="mr-2 h-5 w-5" />
+                        Continue with Google
+                    </Button>
+                    <FieldDescription className="text-center">
+                        Don&apos;t have an account?{" "}
+                        <a href="#" className="underline underline-offset-4">
+                            Sign up
+                        </a>
+                    </FieldDescription>
+                </Field>
+            </FieldGroup>
+        </form>
+    )
+}
